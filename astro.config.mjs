1a7aaba7fad0d8796f1eded03ee9ca1963b4159a
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2024-03-27 22:02:27 +0300

    update

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-// Import /static for a static site
-import netlify from '@astrojs/netlify/functions';
-export default defineConfig({
-  output: "server",
-  adapter: netlify(),
-});
