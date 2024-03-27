7ce7aae8b65b55c28390a0f1d8c12d0669671970
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2024-03-27 21:57:57 +0300

    netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 // Import /static for a static site
-import vercelStatic from '@astrojs/vercel/static';
- 
+import netlify from '@astrojs/netlify/functions';
 export default defineConfig({
-  // Must be 'static' or 'hybrid'
-  output: 'static',
-  adapter: vercelStatic(),
+  output: "server",
+  adapter: netlify(),
 });
